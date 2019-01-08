543608fc052068ff6a675e3a5f82f011b9e6c46b
Robin Lieson <robin.lieson@appschef.com>
2019-01-08 17:15:26 +0700

    add template and routing

---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" name="email" id="email" placeholder="Email" class="form-control">
+    <br>
+    <input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="Password"
+      class="form-control"
+    >
+    <br>
+    <button type="button" class="btn btn-primary" @click="login">Connection</button>
+    <p>You don't have an account ? You can
+      <router-link to="/sign-up">create one</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {
+    login: function() {
+      this.$router.replace("home");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+  padding: 10px 20px;
+}
+h3 {
+  margin-bottom: 25px;
+}
+input {
+  margin: 10px auto;
+  width: 40%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  /* width: 10%; */
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="sign-up">
+    <p>Let's create a new account !</p>
+    <input type="text" name="email" id="email" placeholder="Email" class="form-control">
+    <br>
+    <input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="Password"
+      class="form-control"
+    >
+    <br>
+    <button type="button" class="btn btn-primary">Sign Up</button>
+    <span>or go back to
+      <router-link to="/login">login</router-link>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "signup",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.signUp {
+  margin-top: 40px;
+  padding: 10px 20px;
+}
+h3 {
+  margin-bottom: 25px;
+}
+input {
+  margin: 10px auto;
+  width: 40%;
+  padding: 15px;
+}
+button {
+  margin-top: 10px;
+  /* width: 10%; */
+  cursor: pointer;
+}
+span {
+  display: block;
+  margin-top: 20px;
+  font-size: 11px;
+}
+</style>
