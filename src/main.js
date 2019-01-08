9a78926ec936d6fc455208867f9036e1ee50425c
Robin Lieson <robin.lieson@appschef.com>
2019-01-08 17:22:44 +0700

    add firebase data to apps

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyBAIY57e-iH43s95VEdLkPn8ki9nwAP700",
+  authDomain: "vue-firebase-crud-68c0f.firebaseapp.com",
+  databaseURL: "https://vue-firebase-crud-68c0f.firebaseio.com",
+  projectId: "vue-firebase-crud-68c0f",
+  storageBucket: "vue-firebase-crud-68c0f.appspot.com",
+  messagingSenderId: "1013503918276"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
